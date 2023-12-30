5c9830109cf5b228b9eec0a4230b3dc6c22a1f05
MrArCode <a.c.posluszny@gmail.com>
2023-12-30 15:50:32 +0100

    -Klasa Monster -Medtody do generowania przeciwnków

--- src/LOTR/Main.java
+++ src/LOTR/Main.java
@@ -1,8 +1,6 @@
 package LOTR;
 
 
-import java.lang.reflect.Array;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -20,13 +18,14 @@ public class Main {
 //          Character goblin = Monster.MonsterFactory.createGoblinArcher();
 //          showStats(hero);
 //          showStats(goblin);
-          Monster[] group = {Monster.MonsterFactory.createGoblinMiner(),Monster.MonsterFactory.createGoblinMiner()};
-          System.out.println(group[0].getHealth());
-          System.out.println(group[1].getHealth());
-          System.out.println();
-          group[0].setHealth(1);
-          System.out.println(group[0].getHealth());
-          System.out.println(group[1].getHealth());
+
+//          Monster[] group = {Monster.MonsterFactory.createGoblinMiner(),Monster.MonsterFactory.createGoblinMiner()};
+//          System.out.println(group[0].getHealth());
+//          System.out.println(group[1].getHealth());
+//          System.out.println();
+//          group[0].setHealth(1);
+//          System.out.println(group[0].getHealth());
+//          System.out.println(group[1].getHealth());
 
 
 //        try {
