72155d06980c26543af16fbdfb4f5496bb6e753f
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2024-01-15 23:19:53 +0100

    - Nowe bronie

--- src/LOTR/MagicWeapon.java
+++ src/LOTR/MagicWeapon.java
@@ -10,6 +10,14 @@ public class MagicWeapon extends Weapon {
         public static MagicWeapon createStaff() {
             return new MagicWeapon("Magic Staff", "", generateRandomRarity(), 1000, 2, 0, 0, 40);
         }
+
+        public static MagicWeapon createWand() {
+            return new MagicWeapon("Ethereal Wand", "", generateRandomRarity(), 1200, 3, 0, 0, 50);
+        }
+
+        public static MagicWeapon createWizardStaff() {
+            return new MagicWeapon("Gandalf's Wisdom", "", 5, 5000, 12, 20, 30, 50);
+        }
     }
 }
 
--- src/LOTR/RangeWeapon.java
+++ src/LOTR/RangeWeapon.java
@@ -9,12 +9,26 @@ public class RangeWeapon extends Weapon {
     public static class RangeWeaponFactory {
 
         public static RangeWeapon createBow() {
-            return new RangeWeapon("Bow", "", generateRandomRarity(), 200, 5, 0,30,0);
+            return new RangeWeapon("Bow", "", generateRandomRarity(), 200, 5, 0, 30, 0);
         }
 
         public static RangeWeapon createCrossBow() {
-            return new RangeWeapon("CrossBow", "", generateRandomRarity(), 400, 10, 0,60,0);
+            return new RangeWeapon("CrossBow", "", generateRandomRarity(), 400, 10, 0, 60, 0);
         }
+
+        public static RangeWeapon createThrowingKnife() {
+            return new RangeWeapon("Throwing Knife", "", generateRandomRarity(), 50, 3, 0, 45, 0);
+        }
+
+        public static RangeWeapon createLongbow() {
+            return new RangeWeapon("Longbow", "", generateRandomRarity(), 300, 8, 0, 50, 0);
+        }
+
+        public static RangeWeapon createSling() {
+            return new RangeWeapon("Sling", "", generateRandomRarity(), 100, 2, 0, 20, 0);
+        }
+
+
     }
 
 }
